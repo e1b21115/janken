bba1e1665da2f9537078b047c54bbc68e5bf5a0d
e1b21115 <119831972+e1b21115@users.noreply.github.com>
2023-10-04 09:22:27 +0900

    cpu(グーのみ)との対戦機能の追加

--- src/main/java/oit/is/z2144/janken/controller/JankenController.java
+++ src/main/java/oit/is/z2144/janken/controller/JankenController.java
@@ -13,4 +13,42 @@ public class JankenController {
     model.addAttribute("PlayerName", PlayerName);
     return "janken.html";
   }
+
+  @PostMapping("/janken/start")
+  public String game(@RequestParam String player_hand, ModelMap model) {
+    String[] result_list = { "Draw", "You Win!!", "You Lose..." };
+    String[] hand_list = { "グー", "チョキ", "パー" };
+    String cpu_hand = hand_list[0];
+    String result = "";
+
+    if (player_hand.equals("グー")) {
+      if (cpu_hand.equals("グー")) {
+        result = result_list[0];
+      } else if (cpu_hand.equals("チョキ")) {
+        result = result_list[1];
+      } else if (cpu_hand.equals("パー")) {
+        result = result_list[2];
+      }
+    } else if (player_hand.equals("チョキ")) {
+      if (cpu_hand.equals("グー")) {
+        result = result_list[2];
+      } else if (cpu_hand.equals("チョキ")) {
+        result = result_list[0];
+      } else if (cpu_hand.equals("パー")) {
+        result = result_list[1];
+      }
+    } else if (player_hand.equals("パー")) {
+      if (cpu_hand.equals("グー")) {
+        result = result_list[1];
+      } else if (cpu_hand.equals("チョキ")) {
+        result = result_list[2];
+      } else if (cpu_hand.equals("パー")) {
+        result = result_list[0];
+      }
+    }
+    model.addAttribute("player_hand", player_hand);
+    model.addAttribute("cpu_hand", cpu_hand);
+    model.addAttribute("result", result);
+    return "janken.html";
+  }
 }
